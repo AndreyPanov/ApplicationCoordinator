d0647d515bd2e0634e58049d0e03b92fd9eaf14b
Andrey Panov <apanov@avito.ru>
2016-02-21 23:21:56 +0300

    init commit

---
+++ ApplicationCoordinatorExample/ApplicationCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  ApplicationCoordinator.swift
+//  ApplicationCoordinatorExample
+//
+//  Created by Панов Андрей on 21.02.16.
+//  Copyright © 2016 Andrey Panov. All rights reserved.
+//
+
+import UIKit
+
+class ApplicationCoordinator: NSObject {
+    
+    private(set) var rootController: UINavigationController
+
+    init(rootController: UINavigationController) {
+        
+    }
+}
---
+++ ApplicationCoordinatorExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApplicationCoordinatorExample
+//
+//  Created by Панов Андрей on 21.02.16.
+//  Copyright © 2016 Andrey Panov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
