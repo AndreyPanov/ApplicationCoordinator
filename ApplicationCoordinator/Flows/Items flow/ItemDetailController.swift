f415882bbfad7917391fb9fe5d41b4f04c4582f5
Andrey Panov <panovdev@gmail.com>
2016-04-23 21:27:39 +0300

    Item Detail

--- ApplicationCoordinator/Flows/Items flow/ItemDetailController.swift
+++ ApplicationCoordinator/Flows/Items flow/ItemDetailController.swift
@@ -19,23 +19,6 @@ class ItemDetailController: UIViewController, FlowController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = item?.title ?? "Detail"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
