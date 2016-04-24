153a7e03150e91bb2e596c8961c137f72fdd0259
Andrey Panov <panovdev@gmail.com>
2016-04-24 21:45:15 +0300

    Settings flow

--- ApplicationCoordinator/Flows/Settings flow/SettingsController.swift
+++ ApplicationCoordinator/Flows/Settings flow/SettingsController.swift
@@ -16,29 +16,38 @@ class SettingsController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     //mock datasource
-    var settings = (0...5).map { index in return Setting(title: "Setting № \(index)", subtitle: "Setting descripton") }
+    var settings: [[Setting]] = [[]]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Settings"
+        createMockData()
+    }
+    
+    func createMockData() {
+        
+        let settings1 = (0...3).map { index in return Setting(title: "Setting № \(index)", subtitle: "Setting descripton") }
+        let settings2 = (0...3).map { index in return Setting(title: "Setting № \(index)", subtitle: "Setting descripton") }
+        settings.append(settings1)
+        settings.append(settings2)
     }
 }
 
 extension SettingsController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return settings.count
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return settings.count
+        return settings[section].count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
         
-        let setting = settings[indexPath.row]
+        let setting = settings[indexPath.section][indexPath.row]
         cell.textLabel?.text = setting.title
         cell.detailTextLabel?.text = setting.subtitle
         return cell
