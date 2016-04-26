95378c9dad98ae906c44b7d9a57473e83577fa72
Andrey Panov <apanov@avito.ru>
2016-04-26 18:13:34 +0300

    addDependancy

--- ApplicationCoordinator/Protocols/CoordinatorProtocols.swift
+++ ApplicationCoordinator/Protocols/CoordinatorProtocols.swift
@@ -10,10 +10,30 @@ import Foundation
 
 typealias CoordinatorHandler = () -> ()
 
-protocol Coordinatable: Router {
+protocol Coordinatable: NSObjectProtocol, Router {
     
     var flowCompletionHandler: CoordinatorHandler? {get set}
+    var childCoordinators: [Coordinatable]? {get set}
     func start()
+    func addDependancy(coordinator: Coordinatable)
+    func removeDependancy(coordinator: Coordinatable)
+}
+
+extension Coordinatable {
+    
+    func addDependancy(coordinator: Coordinatable) {
+        
+        if childCoordinators == nil {
+            childCoordinators = []
+        }
+        childCoordinators?.append(coordinator)
+    }
+    
+    func removeDependancy(coordinator: Coordinatable) {
+        guard childCoordinators != nil else { return }
+        
+        
+    }
 }
 
 protocol FlowController: NSObjectProtocol {
