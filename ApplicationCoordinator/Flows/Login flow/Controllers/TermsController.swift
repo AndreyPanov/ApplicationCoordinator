8651b020178675e47a4d0fcef892f1d8aa3ae0c7
Andrey Panov <panovdev@gmail.com>
2016-05-25 21:37:42 +0300

    TermsController

---
+++ ApplicationCoordinator/Flows/Login flow/Controllers/TermsController.swift
@@ -0,0 +1,15 @@
+//
+//  TermsController.swift
+//  ApplicationCoordinator
+//
+//  Created by Andrey Panov on 25/05/16.
+//  Copyright © 2016 Andrey Panov. All rights reserved.
+//
+
+import UIKit
+
+class TermsController: UIViewController {
+
+    @IBAction func termsSwitchValueChanged(sender: UISwitch) {
+    }
+}
