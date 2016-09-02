85345c47d76fe287043152a6dd6b253ff9b9cff4
Andrey Panov <panovdev@gmail.com>
2016-09-02 07:30:16 +0300

    folders

---
+++ ApplicationCoordinatorTests/Router/RouterTest.swift
@@ -0,0 +1,35 @@
+//
+//  RouterTest.swift
+//  ApplicationCoordinator
+//
+//  Created by Андрей on 02.09.16.
+//  Copyright © 2016 Andrey Panov. All rights reserved.
+//
+
+import XCTest
+
+class RouterTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
