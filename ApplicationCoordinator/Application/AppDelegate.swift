794049de29543faf1ee6f2f9e26a8e9426f2d4fd
Andrey Panov <panovdev@gmail.com>
2016-09-17 18:33:00 +0300

    remove foundation import

--- ApplicationCoordinator/Application/AppDelegate.swift
+++ ApplicationCoordinator/Application/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Andrey Panov on 21.02.16.
 //  Copyright © 2016 Andrey Panov. All rights reserved.
 //
+import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
