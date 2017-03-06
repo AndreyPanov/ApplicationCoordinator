d99aa4bcb5ca5409dae76e89d19a5beaa70d7eeb
Andrey Panov <andrey.panov@blacklane.com>
2017-03-06 16:06:44 +0100

    Add PageViewController

--- ApplicationCoordinator/Flows/OnboardingFlow/PageViewController.swift
+++ ApplicationCoordinator/Flows/OnboardingFlow/PageViewController.swift
@@ -1,11 +1,34 @@
 class PageController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
+  
+  var pageViewController: UIPageViewController!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    pageViewController.delegate = self
+    addChildViewController(pageViewController)
+    view.addSubview(pageViewController.view)
+  }
 }
 
-extension PageController: UIPageViewControllerDelegate {
+extension PageController: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
   
+  func pageViewController(_ pageViewController: UIPageViewController,
+                          viewControllerBefore viewController: UIViewController) -> UIViewController? {
+    return UIViewController()
+  }
+  
+  func pageViewController(_ pageViewController: UIPageViewController,
+                                 viewControllerAfter viewController: UIViewController) -> UIViewController? {
+    return UIViewController()
+  }
+  
+  func presentationCount(for pageViewController: UIPageViewController) -> Int {
+    return 3
+  }
+  
+  func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+    return 2
+  }
 }
